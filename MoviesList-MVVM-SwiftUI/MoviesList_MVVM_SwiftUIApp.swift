4cad15303d0ea5066629be47ea77f2af38067276
AhmedMenaim <ahmedmenaim8@gmail.com>
2024-08-04 23:07:16 +0300

    Initial Commit

---
+++ MoviesList-MVVM-SwiftUI/MoviesList_MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesList_MVVM_SwiftUIApp.swift
+//  MoviesList-MVVM-SwiftUI
+//
+//  Created by Menaim on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesList_MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
