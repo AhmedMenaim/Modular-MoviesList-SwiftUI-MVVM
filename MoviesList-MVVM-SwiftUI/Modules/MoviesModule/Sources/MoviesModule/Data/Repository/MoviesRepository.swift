790c27033ba7bcfff56a81fcff9c35525942fece
AhmedMenaim <ahmedmenaim8@gmail.com>
2024-08-07 04:59:26 +0300

    feat(moviesModule) 🚧 Implement caching in movies module.

--- MoviesList-MVVM-SwiftUI/Modules/MoviesModule/Sources/MoviesModule/Data/Repository/MoviesRepository.swift
+++ MoviesList-MVVM-SwiftUI/Modules/MoviesModule/Sources/MoviesModule/Data/Repository/MoviesRepository.swift
@@ -9,15 +9,21 @@ import Foundation
 import Combine
 import MANetwork
 import Commons
+import MoviesCaching
 
 final
 class MoviesRepository {
   // MARK: - Vars
   private var cancellable: Set<AnyCancellable> = []
   private var client: MoviesAPIClientProtocol
+  private var cacheManager: MovieCacheManagerProtocol
 
-  init(client: MoviesAPIClientProtocol) {
+  init(
+    client: MoviesAPIClientProtocol,
+    cacheManager: MovieCacheManagerProtocol
+  ) {
     self.client = client
+    self.cacheManager = cacheManager
   }
 
   // MARK: - Privates
@@ -50,30 +56,70 @@ class MoviesRepository {
       totalPages: response?.totalPages
     )
   }
+
+  private func convert(_ entity: MovieEntity) -> MovieRepositoryModel {
+    return MovieRepositoryModel(
+      posterPath: entity.posterPath,
+      title: entity.title,
+      releaseDate: entity.releaseDate,
+      genreIDs: Array(entity.genreIDs),
+      id: Int(entity.id),
+      voteAverage: entity.voteAverage,
+      voteCount: entity.voteCount,
+      overview: entity.overview
+    )
+  }
+
+  private func convert(_ movie: MovieRepositoryModel) -> MovieEntity {
+    let entity = MovieEntity()
+    entity.id = "\(movie.id ?? 0)"
+    entity.posterPath = movie.posterPath ?? "--"
+    entity.title = movie.title ?? "--"
+    entity.genreIDs.append(objectsIn: movie.genreIDs ?? [])
+    entity.releaseDate = movie.releaseDate ?? Date()
+    entity.overview = movie.overview ?? "--"
+    entity.voteAverage = movie.voteAverage ?? 0
+    entity.voteCount = movie.voteCount ?? 0
+    return entity
+  }
 }
 
 extension MoviesRepository: MoviesRepositoryProtocol {
   func getMovies(for currentPage: Int) -> AnyPublisher<MoviesRepositoryModel?, RepositoryError> {
     return Future { [weak self] promise in
-      guard let self else { return }
-      client.getMovies(for: currentPage)
+      guard let self = self else { return }
+
+      self.client.getMovies(for: currentPage)
         .sink(receiveCompletion: { result in
           if case .failure(let error) = result {
-            promise(.failure(
-              RepositoryError(error: error)
-            ))
+            if RepositoryError(error: error) == RepositoryError.noInternetConnection {
+              let cachedMovies = self.cacheManager.getCachedMovies()
+              if !cachedMovies.isEmpty {
+                let repositoryModels = cachedMovies.map { self.convert($0) }
+                let repositoryModel = MoviesRepositoryModel(
+                  page: currentPage,
+                  movies: repositoryModels,
+                  totalPages: 1
+                )
+                promise(.success(repositoryModel))
+              }
+            } else {
+              promise(.failure(RepositoryError(error: error)))
+            }
           }
         }, receiveValue: { [weak self] response in
-          guard let self else { return }
-          promise(.success(
-            self.convert(response)
-          ))
+          guard let self = self else { return }
+          let repositoryModel = self.convert(response)
+          if let movies = repositoryModel.movies {
+            let entities = movies.map { self.convert($0) }
+            self.cacheManager.cacheMovies(entities)
+          }
+          promise(.success(repositoryModel))
         })
-        .store(in: &cancellable)
-      }
+        .store(in: &self.cancellable)
+    }
     .eraseToAnyPublisher()
   }
-
   func getSearchedMovies(
     with searchedText: String,
     and searchPage: Int
